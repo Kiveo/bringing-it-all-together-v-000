d5480b99602c0c3793689e1e770add432a195dfe
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 07:16:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,10 +67,21 @@ class Dog
     if !result.empty?
       self.reify_from_row(result)
     else
-      dog = self.create(name: name, breed: breed)
+      result = self.create(name: name, breed: breed)
     end
-    dog
+    result 
   end
+  #----------------------
+  def self.find_or_create_by(name:, album:)
+    song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end 
 
 
 end
