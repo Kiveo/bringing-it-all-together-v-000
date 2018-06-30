bff25dfaa48d38c1495a9aa9642838e7e33b3472
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 07:25:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,7 +66,7 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_stats = dog[0]
-      dog = Dog.new(id: dog_stats[0], name: dog_stats[1], breed: dog_stats[2])  #why does it require key: val (not simply the value) elsewhere, but not here?
+      dog = Dog.new(id: dog_stats[0], name: dog_stats[1], breed: dog_stats[2]) 
     else
       dog = self.create(name: name, breed: breed)
     end
