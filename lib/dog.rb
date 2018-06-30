87d92dc397334d99871a047a51ce9f3b36ec8ace
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 06:21:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,7 +16,7 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS dogs (
+      CREATE TABLE IF NOT EXISTS #{self.table_name} (
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT
