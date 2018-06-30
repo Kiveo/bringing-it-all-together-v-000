28d8436e0d118a87aac20fdf56502f3d75f60b53
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 06:09:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,5 +24,17 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-
+  def save #create dog in db, check for duplicates, and assign an ID 
+    if self.id 
+      self.update 
+    else 
+      sql = <<-SQL 
+        INSERT INTO dogs (name, breed) 
+        VALUES (?, ?)
+      SQL 
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end 
+      
 end
