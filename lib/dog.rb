11a38a7dfdd232bd075a1b096e026e1ad0dbe24b
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 06:04:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,10 +19,10 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-  def self.drop_table 
+  def self.drop_table
     sql = "DROP TABLE dogs"
-    DB[:conn].execute(sql) 
-  end 
-  
+    DB[:conn].execute(sql)
+  end
+
 
 end
