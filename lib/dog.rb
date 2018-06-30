34e8b8c1d92c219d213a35ee815f805ac17042ad
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 07:30:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,5 +76,11 @@ class Dog
     self.new(id: row[0], name: row[1], breed: row[2])
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+    dog_row = DB[:conn].execute(sql, name)[0]
+    dog = self.new_from_db(dog_row)
+    # dog 
+  end 
 
 end
